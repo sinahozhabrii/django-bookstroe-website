3228ea22ef0fa0b9727ae7f1e4b3adfb41cc4ffa
sinahozhabrii <sinahzdev@gmail.com>
2024-09-29 16:27:10 +0330

    adding book model to admin and adding __str__ to book model

--- books/models.py
+++ books/models.py
@@ -6,3 +6,6 @@ class Book(models.Model):
     author = models.CharField(max_length=100)
     content = models.TextField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.title} - {self.author}"
