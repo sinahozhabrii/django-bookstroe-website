ef98a3335ed372f30add15c9de1a9b1f7516c590
sinahozhabrii <sinahzdev@gmail.com>
2024-10-01 16:33:56 +0330

    add book update view

--- books/urls.py
+++ books/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path("",views.BookListView.as_view(),name="book_list"),
     path('<int:pk>/',views.BookDetailView.as_view(),name="book_detail"),
+    path('create/',views.BookCreateView.as_view(),name="book_create"),
+    path('<int:pk>/update/',views.BookUpdateView.as_view(),name="book_update"),
 ]
--- books/views.py
+++ books/views.py
@@ -12,3 +12,13 @@ class BookListView(generic.ListView):
 class BookDetailView(generic.DetailView):
     model = Book
     template_name = "books/books_detail_view.html"
+
+class BookCreateView(generic.edit.CreateView):
+    model = Book
+    fields = ['title', 'author', 'content', 'price']
+    template_name = 'books/books_create.html'
+
+class BookUpdateView(generic.UpdateView):
+    model = Book
+    fields = ['title', 'author', 'content', 'price']
+    template_name = "books/books_update.html"
